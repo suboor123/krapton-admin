0ac742ed1d77ba1c307e5f2965256f3e5d6c5263
suboor khan <suboorkhan@suboors-MacBook-Air.local>
2023-06-11 00:11:06 +0530

    tasks init

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { DataSnapshot } from '@angular/fire/database/interfaces';
+import { Observable, from, BehaviorSubject } from 'rxjs';
+import { Task, TaskStatus } from '../types/task';
 
 @Injectable({
     providedIn: 'root',
 })
 export class TaskService {
-    constructor() {}
+    private path: string = 'tasks';
+    constructor(private fb: AngularFireDatabase) {}
+
+    public tasksSubject: BehaviorSubject<Task[]> = new BehaviorSubject(
+        [] as Task[]
+    );
+    public taskObservable$ = this.tasksSubject.asObservable();
+
+    public async create(task: Task): Promise<any> {
+        return this.fb.database
+            .ref(`${this.path}`)
+            .push({ ...task, status: TaskStatus.OPENED });
+    }
+
+    public syncById(id: string): Observable<DataSnapshot> {
+        return from(this.fb.database.ref(`${this.path}/${id}`).get());
+    }
+
+    public syncAll(): Observable<DataSnapshot> {
+        return from(this.fb.database.ref(`${this.path}`).get());
+    }
 }
