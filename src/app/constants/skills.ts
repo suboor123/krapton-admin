db0cc17f8a53569620d0e8d7f6c06c2f2d03cfe4
suboor khan <suboorkhan@suboors-MacBook-Air.local>
2023-06-11 20:58:34 +0530

    skills restruct

--- src/app/constants/skills.ts
+++ src/app/constants/skills.ts
@@ -1,27 +1,21 @@
-export const Skills_Obj = [
+export const SKILLS_OBJ = [
     {
-        id: '1',
         skill: 'html',
     },
     {
-        id: '2',
         skill: 'css',
     },
     {
-        id: '3',
         skill: 'javaScript',
     },
     {
-        id: '1',
         skill: 'Java',
     },
     {
-        id: '1',
         skill: 'Angular',
     },
     {
-        id: '1',
         skill: 'React',
     },
 ];
-export const Skills = Skills_Obj.map((obj) => obj.skill);
+export const Skills = SKILLS_OBJ.map((obj) => obj.skill);
