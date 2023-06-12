ae20da10880a39bea29d40a1c5fc8e1e98eabcad
suboor khan <suboorkhan@suboors-MacBook-Air.local>
2023-06-12 23:19:40 +0530

    task work init

--- src/app/module/task/board/board.component.ts
+++ src/app/module/task/board/board.component.ts
@@ -31,18 +31,16 @@ export class BoardComponent implements OnInit {
 
     ngOnInit(): void {
         this.taskService.refreshAllTasks();
-        this.taskSubscription = this.taskService.taskObservable$.subscribe(
-            (tasks) => {
-                this.tasksMap = {
-                    [TaskStatus.OPENED]: [],
-                    [TaskStatus.IN_PROGESS]: [],
-                    [TaskStatus.IN_REVIEW]: [],
-                    [TaskStatus.COMPLETED]: [],
-                };
-                tasks.forEach((task) => {
-                    this.tasksMap[task.status].push(task);
-                });
-            }
-        );
+        this.taskSubscription = this.taskService.taskObservable$.subscribe((tasks) => {
+            this.tasksMap = {
+                [TaskStatus.OPENED]: [],
+                [TaskStatus.IN_PROGESS]: [],
+                [TaskStatus.IN_REVIEW]: [],
+                [TaskStatus.COMPLETED]: [],
+            };
+            tasks.forEach((task) => {
+                this.tasksMap[task.status].push(task);
+            });
+        });
     }
 }
