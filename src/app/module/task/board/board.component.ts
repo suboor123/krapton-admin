38e6afd9880e60a7de9c85c92d6f9764b1e62fc3
suboor khan <suboorkhan@suboors-MacBook-Air.local>
2023-06-12 23:26:53 +0530

    change budget

--- src/app/module/task/board/board.component.ts
+++ src/app/module/task/board/board.component.ts
@@ -26,18 +26,22 @@ export class BoardComponent implements OnInit {
         [TaskStatus.COMPLETED]: [],
     };
 
-    private taskSubscription: Subscription | undefined;
+    public taskSubscription: Subscription | undefined;
     constructor(private taskService: TaskService) {}
+
+    private resetTaskMap() {
+        this.tasksMap = {
+            [TaskStatus.OPENED]: [],
+            [TaskStatus.IN_PROGESS]: [],
+            [TaskStatus.IN_REVIEW]: [],
+            [TaskStatus.COMPLETED]: [],
+        };
+    }
 
     ngOnInit(): void {
         this.taskService.refreshAllTasks();
         this.taskSubscription = this.taskService.taskObservable$.subscribe((tasks) => {
-            this.tasksMap = {
-                [TaskStatus.OPENED]: [],
-                [TaskStatus.IN_PROGESS]: [],
-                [TaskStatus.IN_REVIEW]: [],
-                [TaskStatus.COMPLETED]: [],
-            };
+            this.resetTaskMap();
             tasks.forEach((task) => {
                 this.tasksMap[task.status].push(task);
             });
