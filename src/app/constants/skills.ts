9c40bc57d6f8fbdd18a8c4fbe0d16d3dab052104
sam5252 <sk397565.sk@gmail.com>
2023-06-13 20:02:52 +0530

    Added skill array

--- src/app/constants/skills.ts
+++ src/app/constants/skills.ts
@@ -1,15 +1,30 @@
 export const SKILLS_OBJ = [
     {
-        skill: 'html',
+        skill: 'Html',
     },
     {
-        skill: 'css',
+        skill: 'Css',
     },
     {
-        skill: 'javaScript',
+        skill: 'JavaScript',
     },
     {
         skill: 'Java',
+    },
+    {
+        skill: 'SQL',
+    },
+    {
+        skill: 'MySQL',
+    },
+    {
+        skill: 'C++',
+    },
+    {
+        skill: 'C#',
+    },
+    {
+        skill: 'Swift',
     },
     {
         skill: 'Angular',
@@ -17,5 +32,137 @@ export const SKILLS_OBJ = [
     {
         skill: 'React',
     },
+    {
+        skill: 'Php',
+    },
+    {
+        skill: 'Docker',
+    },
+    {
+        skill: 'TypeScript',
+    },
+    {
+        skill: 'Redux',
+    },
+    {
+        skill: 'Bootstrap',
+    },
+    {
+        skill: 'MongoDB',
+    },
+    {
+        skill: 'RavenDB',
+    },
+    {
+        skill: 'Django',
+    },
+    {
+        skill: 'AppML',
+    },
+    {
+        skill: 'Asp.Net',
+    },
+    {
+        skill: 'Laravel',
+    },
+    {
+        skill: 'VueJS',
+    },
+    {
+        skill: 'Underscore.JS',
+    },
+    {
+        skill: 'Perl',
+    },
+    {
+        skill: 'Ruby Rails',
+    },
+    {
+        skill: 'Nodejs',
+    },
+    {
+        skill: 'Kotlin',
+    },
+    {
+        skill: 'Rust',
+    },
+    {
+        skill: 'NoSQL',
+    },
+    {
+        skill: 'Scala',
+    },
+    {
+        skill: 'Matlab',
+    },
+    {
+        skill: 'Pascal Script',
+    },
+    {
+        skill: 'Visual Basic .NET',
+    },
+    {
+        skill: 'Fortran',
+    },
+    {
+        skill: 'Cobol',
+    },
+    {
+        skill: 'Lisp',
+    },
+    {
+        skill: 'Dart',
+    },
+    {
+        skill: 'Bash',
+    },
+    {
+        skill: 'Haskell',
+    },
+    {
+        skill: 'jQuery',
+    },
+    {
+        skill: 'Elixir',
+    },
+    {
+        skill: 'Prolog',
+    },
+    {
+        skill: 'Absys',
+    },
+    {
+        skill: 'Datalog',
+    },
+    {
+        skill: 'Alma-0',
+    },
+    {
+        skill: 'CoffeeScript',
+    },
+    {
+        skill: 'D3js',
+    },
+    {
+        skill: 'Objective-C',
+    },
+    {
+        skill: 'Apache Camel',
+    },
+    {
+        skill: 'Ansible',
+    },
+    {
+        skill: 'Aurelia',
+    },
+    
+    {
+        skill: 'Phalcon',
+    },
+   
+    {
+        skill: 'AJAX',
+    },
+
 ];
 export const Skills = SKILLS_OBJ.map((obj) => obj.skill);
